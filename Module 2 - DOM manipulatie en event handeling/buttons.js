30c01d41cde47c75a2651205fb57aa38ca99dd56
john doe <johndoe@example.com>
2023-04-06 15:48:53 +0200

    'commit'

--- Module 2 - DOM manipulatie en event handeling/buttons.js
+++ Module 2 - DOM manipulatie en event handeling/buttons.js
@@ -1,11 +1,19 @@
 function handle_click(event){
-    if(event.target.id == 'knop1' || event.target.id == 'knop2' || event.target.id == 'knop3'){
-        event.target.classList.toggle("new_image");
+    if(event.target.id == 'knop1'){
+        getElementById("container").event.target.classList.toggle("click_1");
+        event.target.classList.toggle("small-image1")
+
+    }
+    else if(event.target.id == 'knop2'){
+        event.target.classList.toggle("click_2");
+        event.target.classList.toggle("small-image2")
     }
     else{
-        event.target.classList.toggle("new_image");
+        event.target.classList.toggle("click_3");
+        event.target.classList.toggle("small-image3")
     }
 }
+
 console.dir(knop1)
 knop1.onclick = handle_click;
 knop2.onclick = handle_click;
