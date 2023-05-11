bc733feade848cbd3bb36812b94d90acd8d8a057
john doe <johndoe@example.com>
2023-05-11 15:22:04 +0200

    'buttons'

--- Module 2 - DOM manipulatie en event handeling/Buttons/buttons.js
+++ Module 2 - DOM manipulatie en event handeling/Buttons/buttons.js
@@ -1,39 +1,11 @@
-value1 = 0
-value2 = 0
-value3 = 0
-function handle_click(event){
-    if(event.target.id == 'knop1'){
-        console.dir(event.target.parentElement);
-        event.target.parentElement.classList.remove("click_2");
-        event.target.parentElement.classList.remove("click_3");
-        event.target.parentElement.classList.add("click_1");
-        document.getElementById("small-image").style.backgroundImage = "url('1.jpg')";
-        value1 +=1 
-        console.log(value1)
-        document.getElementById('knop1').innerHTML=value1
-        }
-    else if(event.target.id == 'knop2'){
-        console.dir(event.target.parentElement);
-        event.target.parentElement.classList.remove("click_1");
-        event.target.parentElement.classList.remove("click_3");
-        event.target.parentElement.classList.add("click_2");
-        document.getElementById("small-image").style.backgroundImage = "url('2.jpg')";
-        value2 +=1 
-        console.log(value2)
-        document.getElementById('knop2').innerHTML=value2
-    }
-    else{
-        console.dir(event.target.parentElement);
-        event.target.parentElement.classList.remove("click_1");
-        event.target.parentElement.classList.remove("click_2");
-        event.target.parentElement.classList.add("click_3");
-        document.getElementById("small-image").style.backgroundImage = "url('3.jpg')";
-        value3 +=1;
-        console.log(value3);
-        document.getElementById('knop3').innerHTML=value3;
-    }
+container = document.getElementById('container')
+function button_click(event){
+    let nr = event.target.id.replace('button','');
+    container.className = '';
+    container.classList.add("click_"+nr);
+    this.value = parseInt(this.value)+1  
 }
-console.dir(knop1)
-knop1.onclick = handle_click;
-knop2.onclick = handle_click;
-knop3.onclick = handle_click;
+
+button1.onclick = button_click;
+button2.onclick = button_click;
+button3.onclick = button_click;
